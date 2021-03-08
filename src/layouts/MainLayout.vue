cc98124938ac05bf948cb01cf89470aba7d2e682
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-03-08 13:19:22 +0200

    feat(layouts.main): update header, add footer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,107 +1,79 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
+    <q-header
+      class="bg-white text-grey-10"
+      bordered
+    >
+      <q-toolbar class="constrain">
         <q-btn
-          flat
-          dense
+          class="large-screen-only"
+          icon="eva-camera-outline"
+          to="/camera"
           round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
+          dense
+          flat
         />
 
-        <q-toolbar-title>
-          Quasar App
+        <q-separator
+          class="large-screen-only"
+          vertical
+          spaced
+        />
+
+        <q-toolbar-title
+          class="text-grand-hotel text-weight-bold"
+        >
+          Quasagram
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn
+          class="large-screen-only"
+          icon="eva-home-outline"
+          to="/"
+          dense
+          round
+          flat
+        />
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer
+      class="small-screen-only bg-white"
+      bordered
+    >
+      <q-tabs
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+      >
+        <q-route-tab
+          to="/"
+          icon="eva-home-outline"
+          exact
+        />
+        <q-route-tab
+          to="/camera"
+          icon="eva-camera-outline"
+        />
+      </q-tabs>
+    </q-footer>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue';
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
-];
-
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
-  data() {
-    return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData,
-    };
-  },
 };
 </script>
+
+<style lang="sass" scoped>
+.q-toolbar__title
+  font-size: 1.5rem
+
+  @media(max-width: $breakpoint-xs-max)
+    text-align: center
+</style>
